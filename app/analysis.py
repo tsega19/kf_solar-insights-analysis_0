cfdcf4410fa187afef144f535b469211c330f45a
tsega19 <113893575+tsega19@users.noreply.github.com>
2024-12-08 17:57:04 +0300

    analysis code added

---
+++ app/analysis.py
@@ -0,0 +1,10 @@
+# analysis.py
+# Import the script and execute functions or code if necessary
+
+# Importing the script as a module (if it contains callable functions)
+# import script
+
+# If the script is standalone, you can execute it directly like this:
+if __name__ == "__main__":
+    # Execute script file
+    exec(open("/mnt/data/script.py").read())
